0c87496eb5bf42a71b1e0e935904652ceb0be345
Sakhawat Ansari <iamsakhawatansari24@gmail.com>
2024-09-09 14:15:11 +0530

    Todo application

---
+++ 10_todoLocalContext/README.md
@@ -0,0 +1,10 @@
+# Todo App
+
+This is a simple Todo application built with React. It allows users to add, edit, complete, and delete tasks.
+
+## Features
+
+- Add new tasks
+- Edit existing tasks
+- Mark tasks as completed
+- Delete tasks
---
+++ 10_todoLocalContext/src/contexts/TodoContext.js
@@ -0,0 +1,23 @@
+import {createContext, useContext} from 'react'
+
+export const TodoContext = createContext({
+    // Values
+    todos: [
+        {
+            id : 1,
+            todo : "Todo msg",
+            completed : false,
+        }
+    ],
+    // Methods
+    addTodo : (todo) => {},
+    updateTodo : (id, todo) => {},
+    deleteTodo : (id) =>{},
+    toggleComplete : (id) => {},
+})
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider 
